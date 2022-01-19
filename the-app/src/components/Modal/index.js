8ebf57eb58ccfe969895325d6ced6cb760fedbaf
buketsoyhan <45885283+buketsoyhan@users.noreply.github.com>
2022-01-19 16:28:02 +0300

    Edited error box display

--- the-app/src/components/Modal/index.js
+++ the-app/src/components/Modal/index.js
@@ -1,13 +1,20 @@
 import React from "react";
+import "./style.css";
 
 function index({ setModal, modal, info }) {
   return (
     <div>
       {modal === true && (
-        <div>
-          <div>{info.username}</div>
-          <div>{info.password}</div>
-          <button onClick={() => setModal(false)}>X</button>
+        <div className="errorBox">
+          <div className="errorBoxTop">
+            <div className="errors"> <strong> Hata</strong></div>
+            <button className="errorIcon" onClick={() => setModal(false)}>X</button>
+          </div>
+
+          <div className="errorName">
+            <div>{info.username}</div>
+            <div>{info.password}</div>
+          </div>
         </div>
       )}
     </div>
